0b5e68e19bd329f6251d7ed901fb9c3d114d19f2
RazvanVC <razvanvc@yahoo.com>
2021-06-17 23:01:55 +0200

    New Windows

--- app/src/main/java/com/sefricam/sefricam3/Pantalla_Menu_Metodos_Y_Captura.java
+++ app/src/main/java/com/sefricam/sefricam3/Pantalla_Menu_Metodos_Y_Captura.java
@@ -161,7 +161,7 @@ public class Pantalla_Menu_Metodos_Y_Captura extends Activity implements View.On
             final Calendar c = Calendar.getInstance();
             Date dt = new Date(System.currentTimeMillis());
             c.setTime(dt);
-            int dia = 1;
+            int dia = c.get(Calendar.DAY_OF_MONTH);
             int mes = c.get(Calendar.MONTH);
             int ano = c.get(Calendar.YEAR);
 
---
+++ app/src/main/java/com/sefricam/sefricam3/Pantalla_Mi_Cuadricula.java
@@ -0,0 +1,4 @@
+package com.sefricam.sefricam3;
+
+public class Pantalla_Mi_Cuadricula {
+}
