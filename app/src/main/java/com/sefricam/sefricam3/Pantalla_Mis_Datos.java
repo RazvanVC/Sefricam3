8a3e99731381a8864991846a7db6dcea900ae49d
RazvanVC <razvanvc@yahoo.com>
2021-09-19 20:36:03 +0200

    Update Pantalla_Mis_Datos.java

--- app/src/main/java/com/sefricam/sefricam3/Pantalla_Mis_Datos.java
+++ app/src/main/java/com/sefricam/sefricam3/Pantalla_Mis_Datos.java
@@ -3,49 +3,62 @@ package com.sefricam.sefricam3;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
-import com.parse.FindCallback;
-import com.parse.ParseException;
+import java.util.Objects;
+
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
 
-import java.util.List;
-
 public class Pantalla_Mis_Datos extends Activity {
 
-    private String email;
+    // UI Parameters
     private TextView tv_NombreDatos, tv_ApellidosDatos, tv_DNIDatos, tv_NumeroGrupoDatos, tv_CapturasDatos, tv_DatosEnviadosDatos;
     private Button btn_VolverDatos;
+
+    //Class Parameters
+    private String email;
     private Limites limites;
 
+    /**
+     * Initialize the screen and all its components
+     * @param savedInstanceState bundle of data that receives when it starts the screen
+     */
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.pantalla_mis_datos);
 
-        iniciarFindView();
+        startFindView();
+
         Bundle datos = this.getIntent().getExtras();
         if (datos != null) {
             email = datos.getString("EMAIL");
             limites = (Limites) datos.getSerializable("LIMITES");
             cargarDatos(email);
         }
-
-
-        btn_VolverDatos.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent activity = new Intent(Pantalla_Mis_Datos.this,Pantalla_Menu_Intermedio.class);
-                activity.putExtra("EMAIL",email);
-                activity.putExtra("LIMITES", limites);
-                startActivity(activity);
-            }
-        });
+        
+        //Sets the click listener and onClick method fo the button
+        btn_VolverDatos.setOnClickListener(v -> onBackPressed());
     }
 
-    private void iniciarFindView() {
+    /**
+     * Called when the activity has detected the user's press of the back
+     * key.  The default implementation simply finishes the current activity,
+     * but you can override this to do whatever you want.
+     */
+    @Override
+    public void onBackPressed() {
+        Intent activity = new Intent(Pantalla_Mis_Datos.this,Pantalla_Menu_Intermedio.class);
+        activity.putExtra("EMAIL",email);
+        activity.putExtra("LIMITES", limites);
+        startActivity(activity);
+    }
+
+    /**
+     * Init the UI elements into the code
+     */
+    private void startFindView() {
         btn_VolverDatos = findViewById(R.id.btn_VolverDatos);
         tv_NombreDatos = findViewById(R.id.tv_NombreDatos);
         tv_ApellidosDatos = findViewById(R.id.tv_ApellidosDatos);
@@ -55,30 +68,30 @@ public class Pantalla_Mis_Datos extends Activity {
         tv_DatosEnviadosDatos = findViewById(R.id.tv_DatosEnviadosDatos);
     }
 
+    /**
+     * Charge the used data in the UI
+     * @param email query parameter to find the user
+     */
     private void cargarDatos(String email) {
-
         ParseQuery<ParseObject> query = ParseQuery.getQuery("_User");
         query.whereEqualTo("username",email);
-        query.findInBackground(new FindCallback<ParseObject>() {
-            @Override
-            public void done(List<ParseObject> objects, ParseException e) {
-                if (e == null){
-                    ParseObject obj = null;
-                    for (int i = 0; i<objects.size(); i++){
-                        obj = objects.get(i);
-                    }
-                    try {
-                        tv_NombreDatos.append(obj.getString("Nombre"));
-                        tv_ApellidosDatos.setText(obj.getString("Apellidos"));
-                        tv_DNIDatos.setText(obj.getString("DNI"));
-                        tv_NumeroGrupoDatos.setText(obj.getNumber("NumGrupo").toString());
-                        tv_CapturasDatos.setText(obj.getNumber("NumAves").toString());
-                        tv_DatosEnviadosDatos.setText(obj.getNumber("NumFichas").toString());
-                    } catch (Exception x){
-                        Toast.makeText(Pantalla_Mis_Datos.this, "ERROR: " + e.getCode(), Toast.LENGTH_LONG).show();
-                    }
-
+        query.findInBackground((objects, e) -> {
+            if (e == null){
+                ParseObject obj = null;
+                for (int i = 0; i<objects.size(); i++){
+                    obj = objects.get(i);
                 }
+                try {
+                    tv_NombreDatos.append(Objects.requireNonNull(obj).getString("Nombre"));
+                    tv_ApellidosDatos.setText(obj.getString("Apellidos"));
+                    tv_DNIDatos.setText(obj.getString("DNI"));
+                    tv_NumeroGrupoDatos.setText(Objects.requireNonNull(obj.getNumber("NumGrupo")).toString());
+                    tv_CapturasDatos.setText(Objects.requireNonNull(obj.getNumber("NumAves")).toString());
+                    tv_DatosEnviadosDatos.setText(Objects.requireNonNull(obj.getNumber("NumFichas")).toString());
+                } catch (Exception x){
+                    Toast.makeText(Pantalla_Mis_Datos.this, "ERROR: " + Objects.requireNonNull(x).getMessage(), Toast.LENGTH_LONG).show();
+                }
+
             }
         });
     }
