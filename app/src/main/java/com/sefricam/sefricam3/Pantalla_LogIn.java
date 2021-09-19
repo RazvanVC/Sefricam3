b91da18b71057477fa3316ada3c93d506e220e9b
RazvanVC <razvanvc@yahoo.com>
2021-09-19 20:46:58 +0200

    Update Pantalla_LogIn.java

--- app/src/main/java/com/sefricam/sefricam3/Pantalla_LogIn.java
+++ app/src/main/java/com/sefricam/sefricam3/Pantalla_LogIn.java
@@ -12,23 +12,27 @@ import com.parse.ParseUser;
 
 public class Pantalla_LogIn extends Activity {
 
+    // UI Parameters
     private EditText email,password;
+
+    // Class Parameters
     private Limites limites;
 
     /**
-     * Crea la pantalla de inicio de sesion
-     * @param savedInstanceState bundle de datos que se recibe de la pantalla anterior
+     * Initialize the screen and all its components
+     * @param savedInstanceState bundle of data that receives when it starts the screen
      */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.pantalla_login);
 
+        // Initialize UI elements
         email = findViewById(R.id.ete_EmailInicio);
         password = findViewById(R.id.etp_ContrasenaIncio);
-
         Button btn_Login = findViewById(R.id.btn_IniciarSesion);
 
+        // Sets onClick method for the button
         btn_Login.setOnClickListener(view -> {
             if (email.getText().toString().isEmpty() || password.getText().toString().isEmpty()){
                 Toast.makeText(Pantalla_LogIn.this, "Los campos no pueden estar vacios", Toast.LENGTH_SHORT).show();
@@ -39,7 +43,7 @@ public class Pantalla_LogIn extends Activity {
     }
 
     /**
-     *
+     * Login the aplication
      * @param emailLogin email que se manda a la BD
      * @param password contraseña del usuario que está intentando registrarse
      */
