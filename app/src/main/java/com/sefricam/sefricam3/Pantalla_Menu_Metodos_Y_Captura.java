a3c182435bad9b0605ea05bc40fdb2e10ea89ed6
RazvanVC <razvanvc@yahoo.com>
2021-10-03 14:29:29 +0200

    Update Pantalla_Menu_Metodos_Y_Captura.java

--- app/src/main/java/com/sefricam/sefricam3/Pantalla_Menu_Metodos_Y_Captura.java
+++ app/src/main/java/com/sefricam/sefricam3/Pantalla_Menu_Metodos_Y_Captura.java
@@ -675,30 +675,19 @@ public class Pantalla_Menu_Metodos_Y_Captura extends Activity implements View.On
      */
     private String cuadricula(){
         //Primer valor
-        String codCuadricula;
-        double x = (envio.getLongitud() - 3.123);
-        double x1 = x/0.1159;
-        double x2 = 12-x1;
-        if(x2<0) x2=0.0;
-        double x3 = Math.ceil(x2);
+        double x = 12-((envio.getLongitud() - 3.123)/0.1159);
+        if(x<0) x=0.0;
+        x = Math.ceil(x);
 
         //Segundo valor
-        double y = (envio.getLatitud() - 39.937133);
-        double y1 = y/0.08878954;
-        double y2 = 14-y1;
-        if(y2<0) y2=0.0;
-        double y3 = Math.ceil(y2);
+        double y = 14-((envio.getLatitud() - 39.937133)/0.08878954);
+        if(y<0) y=0.0;
+        y = Math.ceil(y);
 
         //Juntar los dos numeros
-        int ix3 = (int) x3;
-        int iy3 = (int) y3;
+        int numCuadricula = Integer.parseInt(String.valueOf((int) x).concat(String.valueOf((int) y)));
 
-        String sValor1 = String.valueOf(ix3);
-        String sValor2= String.valueOf(iy3);
-
-        String sValorDef = sValor1.concat(sValor2);
-        int numCuadricula = Integer.parseInt(sValorDef);
-
+        String codCuadricula;
         switch (numCuadricula){
             case 37:
                 codCuadricula = "01UKA10";
@@ -811,7 +800,6 @@ public class Pantalla_Menu_Metodos_Y_Captura extends Activity implements View.On
             case 77:
                 codCuadricula = "37VKA04";
                 break;
-
             case 87:
                 codCuadricula = "38VKA05";
                 break;
@@ -923,7 +911,6 @@ public class Pantalla_Menu_Metodos_Y_Captura extends Activity implements View.On
             case 1111:
                 codCuadricula = "74VKE08";
                 break;
-
             case 1211:
                 codCuadricula = "75VKE09";
                 break;
@@ -960,12 +947,10 @@ public class Pantalla_Menu_Metodos_Y_Captura extends Activity implements View.On
             case 814:
                 codCuadricula = "86VKH05";
                 break;
-
             default:
                 codCuadricula = "ERROR";
                 break;
         }
-
         return codCuadricula;
     }
 }
